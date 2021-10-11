eacece0ec3184be316b108ea5cdb3e5206ddce73
GenilsomAlan <90303422+GenilsomAlan@users.noreply.github.com>
2021-10-11 20:49:26 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,28 @@
+alert("bem vindo ao CalcMaster");
+function insert(num){
+	//recebe o valor do usuario
+	var num1 = document.getElementById('resposta').innerHTML;
+	//imprime simultaneamento no espaço com id = resposta
+	document.getElementById('resposta').innerHTML = num1 + num;
+}
+	//clear apagar tudo no espaço com id = resposta
+function Clear(){
+	document.getElementById('resposta').innerHTML = ' ';
+}
+	//back apaga somente o ultimo numero digitado
+function Back(){
+	//aloca a string digitada
+	var resultado = document.getElementById('resposta').innerHTML;
+	//remove o ultimo digito
+	document.getElementById('resposta').innerHTML = resultado.substring(0, resultado.length -1);
+}
+	//faz os calculos matematicos
+function calculo(){
+	var resultado = document.getElementById('resposta').innerHTML;
+	if (resultado){
+		document.getElementById('resposta').innerHTML = eval(resultado);
+	}
+	else{
+		document.getElementById('resposta').innerHTML = "nada...";
+	}
+}
